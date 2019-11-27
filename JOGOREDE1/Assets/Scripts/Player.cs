f44a0d444efe544f67db3f970407efcfaf123cf8
oliveirajade <oliveiradepaulajaqueline@gmail.com>
2019-11-26 22:10:07 -0300

    Atualização

---
+++ JOGOREDE1/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            transform.Translate(Vector3.forward * 0.05f);
+        }
+
+        if(Input.GetAxis("Vertical") < 0)
+        {
+            transform.Translate(Vector3.back * 0.05f);
+        }
+
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            transform.Rotate(Vector3.up * 0.1f);
+        }
+
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            transform.Rotate(Vector3.up * -0.1f);
+        }
+    }
+}
